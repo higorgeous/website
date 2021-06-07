9ad43fe23da12e350964d38a8f97ca3cd3b3b1ef
Gorgeous BSC token <gorgeous.token@gmail.com>
2021-06-07 05:33:46 +0000

    Update supply chart

--- src/components/Library/Page/components/Section/components/SupplyChart/index.tsx
+++ src/components/Library/Page/components/Section/components/SupplyChart/index.tsx
@@ -1,147 +1,103 @@
 import React from 'react';
 import { ResponsivePie } from '@nivo/pie';
 
+import { Wrapper } from './styles';
+
 const data = [
   {
-    id: `Burn wallet (50%)`,
+    id: `burn`,
     label: `Burn wallet (50%)`,
-    value: 50000000,
-    color: `hsl(19, 70%, 50%)`,
+    value: 50,
   },
   {
-    id: `Charity wallet (4%)`,
+    id: `community`,
+    label: `Community`,
+    value: 38,
+  },
+  {
+    id: `charity`,
     label: `Charity wallet (4%)`,
-    value: 4000000,
-    color: `hsl(213, 70%, 50%)`,
+    value: 4,
   },
   {
-    id: `Project wallet (4%)`,
-    label: `Projec wallet (4%)`,
-    value: 4000000,
-    color: `hsl(58, 70%, 50%)`,
+    id: `project`,
+    label: `Project wallet (4%)`,
+    value: 4,
   },
   {
-    id: `Dev wallet (4%)`,
-    label: `Dev wallet (4%)`,
-    value: 4000000,
-    color: `#d9268e`,
+    id: `team`,
+    label: `Team wallet (4%)`,
+    value: 4,
   },
 ];
 
 const SupplyChart: React.FC = () => (
-  <ResponsivePie
-    data={data}
-    margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
-    innerRadius={0.5}
-    padAngle={0.7}
-    cornerRadius={3}
-    // colors={{ scheme: `nivo` }}
-    borderWidth={1}
-    borderColor={{ from: `color`, modifiers: [[`darker`, 0.2]] }}
-    radialLabelsSkipAngle={10}
-    radialLabelsTextXOffset={6}
-    radialLabelsTextColor="#333333"
-    radialLabelsLinkOffset={0}
-    radialLabelsLinkDiagonalLength={16}
-    radialLabelsLinkHorizontalLength={24}
-    radialLabelsLinkStrokeWidth={1}
-    radialLabelsLinkColor={{ from: `color` }}
-    slicesLabelsSkipAngle={10}
-    slicesLabelsTextColor="#333333"
-    animate
-    motionStiffness={90}
-    motionDamping={15}
-    defs={[
-      {
-        id: `dots`,
-        type: `patternDots`,
-        background: `inherit`,
-        color: `rgba(255, 255, 255, 0.3)`,
-        size: 4,
-        padding: 1,
-        stagger: true,
-      },
-      {
-        id: `lines`,
-        type: `patternLines`,
-        background: `inherit`,
-        color: `rgba(255, 255, 255, 0.3)`,
-        rotation: -45,
-        lineWidth: 6,
-        spacing: 10,
-      },
-    ]}
-    fill={[
-      {
-        match: {
-          id: `ruby`,
+  <Wrapper>
+    <ResponsivePie
+      data={data}
+      margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+      sortByValue
+      innerRadius={0.4}
+      padAngle={3}
+      cornerRadius={3}
+      colors={{ scheme: `purpleRed_green` }}
+      borderWidth={2}
+      borderColor={{ from: `color`, modifiers: [[`darker`, 0.3]] }}
+      enableArcLinkLabels={false}
+      // Arc Labels
+      arcLabel={(e) => `${e.id} (${e.value})`}
+      arcLabelsSkipAngle={10}
+      arcLabelsTextColor={{ theme: `background` }}
+      defs={[
+        {
+          id: `dots`,
+          type: `patternDots`,
+          background: `inherit`,
+          color: `rgba(255, 255, 255, 0.3)`,
+          size: 4,
+          padding: 1,
+          stagger: true,
         },
-        id: `dots`,
-      },
-      {
-        match: {
-          id: `c`,
+        {
+          id: `lines`,
+          type: `patternLines`,
+          background: `inherit`,
+          color: `rgba(255, 255, 255, 0.3)`,
+          rotation: -45,
+          lineWidth: 6,
+          spacing: 10,
         },
-        id: `dots`,
-      },
-      {
-        match: {
-          id: `go`,
-        },
-        id: `dots`,
-      },
-      {
-        match: {
-          id: `python`,
-        },
-        id: `dots`,
-      },
-      {
-        match: {
-          id: `scala`,
-        },
-        id: `lines`,
-      },
-      {
-        match: {
-          id: `lisp`,
-        },
-        id: `lines`,
-      },
-      {
-        match: {
-          id: `elixir`,
-        },
-        id: `lines`,
-      },
-      {
-        match: {
-          id: `javascript`,
-        },
-        id: `lines`,
-      },
-    ]}
-    legends={[
-      {
-        anchor: `bottom`,
-        direction: `row`,
-        translateY: 50,
-        itemWidth: 100,
-        itemHeight: 18,
-        itemTextColor: `#999`,
-        symbolSize: 18,
-        symbolShape: `circle`,
-        effects: [
-          {
-            on: `hover`,
-            style: {
-              itemTextColor: `#000`,
-            },
+      ]}
+      fill={[
+        {
+          match: {
+            id: `burn`,
           },
-        ],
-      },
-    ]}
-  />
+          id: `lines`,
+        },
+        {
+          match: {
+            id: `charity`,
+          },
+          id: `dots`,
+        },
+        {
+          match: {
+            id: `project`,
+          },
+          id: `dots`,
+        },
+        {
+          match: {
+            id: `team`,
+          },
+          id: `dots`,
+        },
+      ]}
+      isInteractive={false}
+      legends={[]}
+    />
+  </Wrapper>
 );
 
 export default SupplyChart;
